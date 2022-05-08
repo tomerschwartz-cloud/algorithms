201ed154fa8db1face6122a633875c994d192509
tomerschwartz-cloud <62577162+tomerschwartz-cloud@users.noreply.github.com>
2022-05-08 18:23:32 +0300

    Add files via upload
    Course introduction exerciese

---
+++ algorithms1/src/RandomWord.java
@@ -0,0 +1,20 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdOut;
+
+public class RandomWord {
+	public static void main(String[] argv) {
+		String champion = "";
+		int i = 1;
+		
+		while(!StdIn.isEmpty()) {
+			if(StdRandom.bernoulli(1.0/(i++))) {
+				champion = StdIn.readString();
+			}
+			else{
+				StdIn.readString();
+			}
+		}
+		StdOut.println(champion);
+	}
+}
